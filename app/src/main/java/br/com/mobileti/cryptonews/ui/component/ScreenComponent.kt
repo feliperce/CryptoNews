fd2f82386ad2a372a6760b201e96b2c6a0723aa5
Felipe Rodrigues <felipe.rce@gmail.com>
2021-12-16 18:47:33 -0300

    Home screen composable

---
+++ app/src/main/java/br/com/mobileti/cryptonews/ui/component/ScreenComponent.kt
@@ -0,0 +1,24 @@
+package br.com.mobileti.cryptonews.ui.component
+
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.res.stringResource
+import br.com.mobileti.cryptonews.ui.theme.Purple500
+
+@Composable
+fun CryptoNewsAppBar(
+    title: Int,
+    navigationIcon: @Composable() (() -> Unit)? = null,
+    actions: @Composable (RowScope.() -> Unit)? = null
+) {
+    TopAppBar(
+        title = { Text(stringResource(id = title)) },
+        backgroundColor = Purple500,
+        contentColor = White,
+        navigationIcon = navigationIcon,
+        actions = { actions?.invoke(this) }
+    )
+}
--- app/src/main/java/br/com/mobileti/cryptonews/ui/theme/Type.kt
+++ app/src/main/java/br/com/mobileti/cryptonews/ui/theme/Type.kt
@@ -6,23 +6,26 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+        fontSize = TextSizeMedium
     ),
-    caption = TextStyle(
+    body2 = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+        fontSize = TextSizeSmall
+    ),
+    subtitle1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = TextSizeMedium
+    ),
+    h6 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Bold,
+        fontSize = TextSizeLarge
     )
-    */
+
 )
