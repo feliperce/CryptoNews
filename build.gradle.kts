e042c5d046f41fe1cf42d5c88b0ccd515f59a06b
Felipe Rodrigues <felipe.rce@gmail.com>
2022-02-21 22:42:53 -0300

    Gradle upgrade to 7.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.4")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}")
+        classpath(Dependencies.Project.gradle)
+        classpath(Dependencies.Project.kotlin)
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
